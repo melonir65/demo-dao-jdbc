288276228bf46f97f1b865f39fadf80756066096
RafaelMeloni <rafaelmeloni@gmail.com>
2025-03-23 21:48:38 -0300

    Seller entity class

--- src/application/Program.java
+++ src/application/Program.java
@@ -2,14 +2,19 @@ package application;
 
 
 import model.entities.Department;
+import model.entities.Seller;
 
-
+import java.util.Date;
 
 
 public class Program {
     public static void main(String[] args) {
 
         Department department = new Department(1,"Books");
-        System.out.println(department);
+
+        Seller seller = new Seller(1,"Rafael","rafaelmeloni@gmail.com",new Date(),4000.50,department);
+        System.out.println(seller);
+
+
     }
 }
