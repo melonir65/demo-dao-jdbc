e49d35c78c09cd00302046474cc7dfc997d4ec9e
RafaelMeloni <rafaelmeloni@gmail.com>
2025-03-23 21:39:34 -0300

    Department entity class

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,12 +1,15 @@
 package application;
 
-import db.DB;
-import java.sql.*;
+
+import model.entities.Department;
+
+
 
 
 public class Program {
     public static void main(String[] args) {
 
-        System.out.println("iniciando projeto");
+        Department department = new Department(1,"Books");
+        System.out.println(department);
     }
 }
