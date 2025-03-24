7463bd26a5efd73124fb09bf997172d03cf70b85
RafaelMeloni <rafaelmeloni@gmail.com>
2025-03-23 21:32:19 -0300

    Creating project and git repository

---
+++ src/application/Program.java
@@ -0,0 +1,12 @@
+package application;
+
+import db.DB;
+import java.sql.*;
+
+
+public class Program {
+    public static void main(String[] args) {
+
+        System.out.println("iniciando projeto");
+    }
+}
